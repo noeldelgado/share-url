fbcc6518d6a739d4b0966438441ac6ff5fe0628d
Noel Delgado <auhcsei@gmail.com>
2017-07-12 20:47:45 -0500

    update indentation, from 4 to 2

--- index.js
+++ index.js
@@ -4,132 +4,132 @@
  * @license MIT
  */
 (function(root, factory) {
-    if (typeof exports === 'object') module.exports = factory(root);
-    else root.ShareUrl = factory(root);
+  if (typeof exports === 'object') module.exports = factory(root);
+  else root.ShareUrl = factory(root);
 }(this, function factory(root) {
-    var ENDPOINTS = {
-        facebook    : 'https://www.facebook.com/sharer/sharer.php?',
-        twitter     : 'https://twitter.com/share?',
-        googlePlus  : 'https://plus.google.com/share?',
-        pinterest   : 'https://pinterest.com/pin/create/button/?',
-        reddit      : 'http://www.reddit.com/submit?',
-        delicious   : 'https://delicious.com/save?',
-        linkedin    : 'https://www.linkedin.com/shareArticle?'
-    };
+  var ENDPOINTS = {
+    facebook    : 'https://www.facebook.com/sharer/sharer.php?',
+    twitter     : 'https://twitter.com/share?',
+    googlePlus  : 'https://plus.google.com/share?',
+    pinterest   : 'https://pinterest.com/pin/create/button/?',
+    reddit      : 'http://www.reddit.com/submit?',
+    delicious   : 'https://delicious.com/save?',
+    linkedin    : 'https://www.linkedin.com/shareArticle?'
+  };
 
-    return {
-        facebook    : facebook,
-        twitter     : twitter,
-        googlePlus  : googlePlus,
-        pinterest   : pinterest,
-        reddit      : reddit,
-        delicious   : delicious,
-        linkedin    : linkedin,
-        email       : email
-    };
+  return {
+    facebook    : facebook,
+    twitter     : twitter,
+    googlePlus  : googlePlus,
+    pinterest   : pinterest,
+    reddit      : reddit,
+    delicious   : delicious,
+    linkedin    : linkedin,
+    email       : email
+  };
 
-    function _generateUrlParams(data) {
-        return Object.keys(data || {}).map(function(propertyName) {
-            return propertyName + '=' + encodeURIComponent(data[propertyName]);
-        }).join('&');
-    }
+  function _generateUrlParams(data) {
+    return Object.keys(data || {}).map(function(propertyName) {
+      return propertyName + '=' + encodeURIComponent(data[propertyName]);
+    }).join('&');
+  }
 
-    /* Compose the share on facebook url string.
-     * @argument data [Object] <required>
-     * @argument data.u [String] <required>
-     * @return url
-     */
-    function facebook(data) {
-        return ENDPOINTS.facebook + _generateUrlParams(data);
-    }
+  /* Compose the share on facebook url string.
+   * @argument data [Object] <required>
+   * @argument data.u [String] <required>
+   * @return url
+   */
+  function facebook(data) {
+    return ENDPOINTS.facebook + _generateUrlParams(data);
+  }
 
-    /* Compose the share on twitter url string.
-     * @argument data [Object] <required>
-     * @argument data.text [String] <optional> Pre-populated text highlighted in the Tweet composer.
-     * @argument data.in_reply_to [String] <optional> Status ID string of a parent Tweet such as a Tweet from your account (if applicable).
-     * @argument data.url [String] <optional> URL included with the Tweet.
-     * @argument data.hashtags [String] <optional> A comma-separated list of hashtags to be appended to default Tweet text.
-     * @argument data.via [String] <optional> Attribute the source of a Tweet to a Twitter username.
-     * @argument data.related [String] <optional> A comma-separated list of accounts related to the content of the shared URI.
-     * @info https://dev.twitter.com/web/tweet-button/parameters
-     * @return url
-     */
-    function twitter(data) {
-        return ENDPOINTS.twitter + _generateUrlParams(data);
-    }
+  /* Compose the share on twitter url string.
+   * @argument data [Object] <required>
+   * @argument data.text [String] <optional> Pre-populated text highlighted in the Tweet composer.
+   * @argument data.in_reply_to [String] <optional> Status ID string of a parent Tweet such as a Tweet from your account (if applicable).
+   * @argument data.url [String] <optional> URL included with the Tweet.
+   * @argument data.hashtags [String] <optional> A comma-separated list of hashtags to be appended to default Tweet text.
+   * @argument data.via [String] <optional> Attribute the source of a Tweet to a Twitter username.
+   * @argument data.related [String] <optional> A comma-separated list of accounts related to the content of the shared URI.
+   * @info https://dev.twitter.com/web/tweet-button/parameters
+   * @return url
+   */
+  function twitter(data) {
+    return ENDPOINTS.twitter + _generateUrlParams(data);
+  }
 
-    /* Compose the share on google+ url string.
-     * @argument data [Object] <required>
-     * @argument data.url [String] <required> The URL of the page to share.
-     * @info https://developers.google.com/+/web/share/
-     * @return url
-     */
-    function googlePlus(data) {
-        return ENDPOINTS.googlePlus + _generateUrlParams(data);
-    }
+  /* Compose the share on google+ url string.
+   * @argument data [Object] <required>
+   * @argument data.url [String] <required> The URL of the page to share.
+   * @info https://developers.google.com/+/web/share/
+   * @return url
+   */
+  function googlePlus(data) {
+    return ENDPOINTS.googlePlus + _generateUrlParams(data);
+  }
 
-    /* Compose the share on pinterest url string.
-     * @argument data [Object] <required>
-     * @argument data.url <required>
-     * @argument data.media <required>
-     * @argument data.description <required>
-     * @info https://developers.pinterest.com/pin_it/
-     * @return url
-     */
-    function pinterest(data) {
-        return ENDPOINTS.pinterest + _generateUrlParams(data);
-    }
+  /* Compose the share on pinterest url string.
+   * @argument data [Object] <required>
+   * @argument data.url <required>
+   * @argument data.media <required>
+   * @argument data.description <required>
+   * @info https://developers.pinterest.com/pin_it/
+   * @return url
+   */
+  function pinterest(data) {
+    return ENDPOINTS.pinterest + _generateUrlParams(data);
+  }
 
-    /* Compose the submit to reddit url string.
-     * @argument data [Object] <required>
-     * @argument data.url <required>
-     * @argument data.title <optional>
-     * @info http://www.reddit.com/buttons/
-     * @return url
-     */
-    function reddit(data) {
-        return ENDPOINTS.reddit + _generateUrlParams(data);
-    }
+  /* Compose the submit to reddit url string.
+   * @argument data [Object] <required>
+   * @argument data.url <required>
+   * @argument data.title <optional>
+   * @info http://www.reddit.com/buttons/
+   * @return url
+   */
+  function reddit(data) {
+    return ENDPOINTS.reddit + _generateUrlParams(data);
+  }
 
-    /* Compose the url string to post on delicious.
-     * @argument data [Object] <required>
-     * @argument url [String] <required>
-     * @argument title [String] <optional>
-     * @info https://delicious.com/tools
-     * @return url
-     */
-    function delicious(data) {
-        return ENDPOINTS.delicious + _generateUrlParams(data);
-    }
+  /* Compose the url string to post on delicious.
+   * @argument data [Object] <required>
+   * @argument url [String] <required>
+   * @argument title [String] <optional>
+   * @info https://delicious.com/tools
+   * @return url
+   */
+  function delicious(data) {
+    return ENDPOINTS.delicious + _generateUrlParams(data);
+  }
 
-    /* Compose the share article on linkedin url string.
-     * @argument data [Object] <required>
-     * @argument data.url [String, 1024] <required> The url-encoded URL of the page that you wish to share.
-     * @argument data.mini [Boolean] <required> A required argument who's value must always be: true
-     * @argument title [String, 200] <optional> The url-encoded title value that you wish you use.
-     * @argument summary [String, 256] <optional> The url-encoded description that you wish you use.
-     * @argument source [String, 200] <optional> The url-encoded source of the content (e.g. your website or application name)
-     * @info https://developer.linkedin.com/docs/share-on-linkedin
-     * @return url
-     */
-    function linkedin(data) {
-        return ENDPOINTS.linkedin + _generateUrlParams(data);
-    }
+  /* Compose the share article on linkedin url string.
+   * @argument data [Object] <required>
+   * @argument data.url [String, 1024] <required> The url-encoded URL of the page that you wish to share.
+   * @argument data.mini [Boolean] <required> A required argument who's value must always be: true
+   * @argument title [String, 200] <optional> The url-encoded title value that you wish you use.
+   * @argument summary [String, 256] <optional> The url-encoded description that you wish you use.
+   * @argument source [String, 200] <optional> The url-encoded source of the content (e.g. your website or application name)
+   * @info https://developer.linkedin.com/docs/share-on-linkedin
+   * @return url
+   */
+  function linkedin(data) {
+    return ENDPOINTS.linkedin + _generateUrlParams(data);
+  }
 
-    /* Compose the send email url string.
-     * @argument data [Object] <required>
-     * @argument to [String] <required>
-     * @argument subject [String] <optional>
-     * @argument cc [String] <optional>
-     * @argument bcc [String] <optional>
-     * @argument body [String] <optional>
-     * @info https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Email_links
-     * @return url
-     */
-    function email(data) {
-        var to = data.to;
-        delete data.to;
-        var params = _generateUrlParams(data);
-        return 'mailto:' + (params.length ? (to + '?' + params) : to);
-    }
+  /* Compose the send email url string.
+   * @argument data [Object] <required>
+   * @argument to [String] <required>
+   * @argument subject [String] <optional>
+   * @argument cc [String] <optional>
+   * @argument bcc [String] <optional>
+   * @argument body [String] <optional>
+   * @info https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Email_links
+   * @return url
+   */
+  function email(data) {
+    var to = data.to;
+    delete data.to;
+    var params = _generateUrlParams(data);
+    return 'mailto:' + (params.length ? (to + '?' + params) : to);
+  }
 }));
--- test/test.js
+++ test/test.js
@@ -1,90 +1,90 @@
-var assert = require('assert');
-var shareUrl = require('../');
+const assert = require('assert');
+const shareUrl = require('../');
 
 describe('Facebook', function() {
-    it('Compose the share on facebook url string.', function() {
-        var u = 'https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fgoogle.com';
-        assert(shareUrl.facebook({
-            u: 'http://google.com'
-        }) === u);
-    });
+  it('Compose the share on facebook url string.', function() {
+    const u = 'https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fgoogle.com';
+    assert(shareUrl.facebook({
+      u: 'http://google.com'
+    }) === u);
+  });
 });
 
 describe('Twitter', function() {
-    it('Compose the share on twitter url string.', function() {
-        var u = 'https://twitter.com/share?text=custom%20share%20text&in_reply_to=471716611724812288&url=https%3A%2F%2Fdev.twitter.com%2Fweb%2Ftweet-button&hashtags=example%2Cdemo&via=twitterdev&related=twitterapi%2Ctwitter';
-        assert(shareUrl.twitter({
-            text: 'custom share text',
-            in_reply_to: '471716611724812288',
-            url: 'https://dev.twitter.com/web/tweet-button',
-            hashtags: 'example,demo',
-            via: 'twitterdev',
-            related: 'twitterapi,twitter'
-        }) === u);
-    });
+  it('Compose the share on twitter url string.', function() {
+    const u = 'https://twitter.com/share?text=custom%20share%20text&in_reply_to=471716611724812288&url=https%3A%2F%2Fdev.twitter.com%2Fweb%2Ftweet-button&hashtags=example%2Cdemo&via=twitterdev&related=twitterapi%2Ctwitter';
+    assert(shareUrl.twitter({
+      text: 'custom share text',
+      in_reply_to: '471716611724812288',
+      url: 'https://dev.twitter.com/web/tweet-button',
+      hashtags: 'example,demo',
+      via: 'twitterdev',
+      related: 'twitterapi,twitter'
+    }) === u);
+  });
 });
 
 describe('Google+', function() {
-    it('Compose the share on google+ url string.', function() {
-        var u = 'https://plus.google.com/share?url=http%3A%2F%2Fgoogle.com';
-        assert(shareUrl.googlePlus({
-            url: 'http://google.com'
-        }) === u);
-    });
+  it('Compose the share on google+ url string.', function() {
+    const u = 'https://plus.google.com/share?url=http%3A%2F%2Fgoogle.com';
+    assert(shareUrl.googlePlus({
+      url: 'http://google.com'
+    }) === u);
+  });
 });
 
 describe('Pinterest', function() {
-    it('Compose the share on pinterest url string.', function() {
-        var u = 'https://pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.flickr.com%2Fphotos%2Fkentbrew%2F6851755809%2F&media=http%3A%2F%2Ffarm8.staticflickr.com%2F7027%2F6851755809_df5b2051c9_z.jpg&description=Next%20Stop%20Pinterest';
-        assert(shareUrl.pinterest({
-            url : 'http://www.flickr.com/photos/kentbrew/6851755809/',
-            media : 'http://farm8.staticflickr.com/7027/6851755809_df5b2051c9_z.jpg',
-            description : 'Next Stop Pinterest'
-        }) === u);
-    });
+  it('Compose the share on pinterest url string.', function() {
+    const u = 'https://pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.flickr.com%2Fphotos%2Fkentbrew%2F6851755809%2F&media=http%3A%2F%2Ffarm8.staticflickr.com%2F7027%2F6851755809_df5b2051c9_z.jpg&description=Next%20Stop%20Pinterest';
+    assert(shareUrl.pinterest({
+      url : 'http://www.flickr.com/photos/kentbrew/6851755809/',
+      media : 'http://farm8.staticflickr.com/7027/6851755809_df5b2051c9_z.jpg',
+      description : 'Next Stop Pinterest'
+    }) === u);
+  });
 });
 
 describe('Reddit', function() {
-    it('Compose the submit to reddit url string.', function() {
-        var u = 'http://www.reddit.com/submit?url=%2F%2Fwww.reddit.com%2Fbuttons&title=Buttons!';
-        assert(shareUrl.reddit({
-            url : '//www.reddit.com/buttons',
-            title : 'Buttons!'
-        }) === u);
-    });
+  it('Compose the submit to reddit url string.', function() {
+    const u = 'http://www.reddit.com/submit?url=%2F%2Fwww.reddit.com%2Fbuttons&title=Buttons!';
+    assert(shareUrl.reddit({
+      url : '//www.reddit.com/buttons',
+      title : 'Buttons!'
+    }) === u);
+  });
 });
 
 describe('Delicious', function() {
-    it('Compose the url string to post on delicious.', function() {
-        var u = 'https://delicious.com/save?url=https%3A%2F%2Fdelicious.com%2Ftools&title=Delicious';
-        assert(shareUrl.delicious({
-            url : 'https://delicious.com/tools',
-            title : 'Delicious'
-        }) === u);
-    });
+  it('Compose the url string to post on delicious.', function() {
+    const u = 'https://delicious.com/save?url=https%3A%2F%2Fdelicious.com%2Ftools&title=Delicious';
+    assert(shareUrl.delicious({
+      url : 'https://delicious.com/tools',
+      title : 'Delicious'
+    }) === u);
+  });
 });
 
 describe('Linkedin', function() {
-    it('', function() {
-        var u = 'https://www.linkedin.com/shareArticle?mini=true&url=http%3A%2F%2Fdeveloper.linkedin.com&title=LinkedIn%20Developer%20Network&summary=My%20favorite%20developer%20program&source=LinkedIn';
-        assert(shareUrl.linkedin({
-            mini : true,
-            url : 'http://developer.linkedin.com',
-            title : 'LinkedIn Developer Network',
-            summary : 'My favorite developer program',
-            source : 'LinkedIn'
-        }) === u);
-    });
+  it('', function() {
+    const u = 'https://www.linkedin.com/shareArticle?mini=true&url=http%3A%2F%2Fdeveloper.linkedin.com&title=LinkedIn%20Developer%20Network&summary=My%20favorite%20developer%20program&source=LinkedIn';
+    assert(shareUrl.linkedin({
+      mini : true,
+      url : 'http://developer.linkedin.com',
+      title : 'LinkedIn Developer Network',
+      summary : 'My favorite developer program',
+      source : 'LinkedIn'
+    }) === u);
+  });
 });
 
 describe('Email', function() {
-    it('Compose the send email url string.', function() {
-       var u = 'mailto:nowhere@mozilla.org?cc=nobody%40mozilla.org&subject=This%20is%20the%20subject&body=This%20is%20the%20body%20-%20https%3A%2F%2Fdeveloper.mozilla.org%2F';
-       assert(shareUrl.email({
-           to : 'nowhere@mozilla.org',
-           cc : 'nobody@mozilla.org',
-           subject : 'This is the subject',
-           body : 'This is the body - https://developer.mozilla.org/'
-       }) === u);
-    });
+  it('Compose the send email url string.', function() {
+    const u = 'mailto:nowhere@mozilla.org?cc=nobody%40mozilla.org&subject=This%20is%20the%20subject&body=This%20is%20the%20body%20-%20https%3A%2F%2Fdeveloper.mozilla.org%2F';
+    assert(shareUrl.email({
+      to : 'nowhere@mozilla.org',
+      cc : 'nobody@mozilla.org',
+      subject : 'This is the subject',
+      body : 'This is the body - https://developer.mozilla.org/'
+    }) === u);
+  });
 });
